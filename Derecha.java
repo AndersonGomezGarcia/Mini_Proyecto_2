efbbd996ee0e9768f4f5f3ebe434d58cc5bdc17f
JDaniel05 <jose.daniel.ceballos@correounivalle.edu.co>
2023-10-03 13:01:35 -0500

    partidos derecha

---
+++ Derecha.java
@@ -0,0 +1,17 @@
+public enum Derecha {
+   
+    Partido_Liberal_Colombiano, Partido_Conservador_Colombiano,
+    Liga_de_Gobernantes_Anticorrupción,
+    Movimiento_Autoridades_Indígenas_de_Colombia,
+    Partido_Verde_Oxígeno,
+    Unión_Patriótica,
+    Salvación_Nacional,
+    Partido_Alianza_Social_Independiente,
+    Partido_Cambio_Radical,
+    Partido_Político_Mira
+    
+
+      
+}
+
+
---
+++ Izquierda.java
@@ -0,0 +1,13 @@
+public enum Izquierda {
+    Partido_de_la_U,
+    Partido_Alianza_Verde,
+    Partido_Polo_Democrático_Alternativo,
+    Colombia_Humana,
+    Partido_Centro_Democrático,
+    Movimiento_Alternativo_Indígena_y_Social,
+    Partido_Colombia_Justa_Libres,
+    Partido_Colombia_Renaciente,
+    Partido_ADA,
+    Partido_Dignidad_y_Compromiso,
+}
+
